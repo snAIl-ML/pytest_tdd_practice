1a2e5a2351d663855fb7c7e9744a03b60988e6bc
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-05-07 18:17:35 +0100

    Fixed 4/5 pylint violations in wallet.py

--- temp/wallet.py
+++ temp/wallet.py
@@ -9,9 +9,11 @@ class Wallet(object):
         self.balance = balance
 
     def add_cash(self, amount):
+        "Addition of money"
         self.balance += amount
 
     def spend_cash(self, amount):
+        ""
         if self.balance < amount:
             raise InsufficientAmount('You do not have that much money')
         self.balance -= amount
