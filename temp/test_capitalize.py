a7c3e1b83f2362c1562ca149632755cd493481d5
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-05-08 22:39:37 +0100

    Fix 2/5 pylint violations

--- temp/test_capitalize.py
+++ temp/test_capitalize.py
@@ -4,6 +4,7 @@ import pytest
 from capital_case import *
 
 def test_capital_case():
+    'Main purpose of function'
     assert capital_case('semaphore') == 'Semaphore'
 
 def test_raises_exception_on_non_string_arguments():
