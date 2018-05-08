5b756206b856c87554ca0f479404fee0d5270c44
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-05-08 22:36:03 +0100

    Fix 2/3 pylint violations

--- temp/capital_case.py
+++ temp/capital_case.py
@@ -1,5 +1,6 @@
 'Capital example'
 def capital_case(x):
+    'Function example'
     if not isinstance(x, str):
         raise TypeError('Please provide a string argument')
     return x.capitalize()
